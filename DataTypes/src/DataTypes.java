bf8623ed8afa8c1cd712afe8fe352692858849ab
Leandro Valladolid <leandro@valladolid.systems>
2021-12-22 23:11:20 -0500

    Subiendo DataTypes

--- DataTypes/src/DataTypes.java
+++ DataTypes/src/DataTypes.java
@@ -1,6 +1,16 @@
 public class DataTypes {
     public static void main(String[] args) {
         int n = 1234567890;
+        //Se coloca la L al final para que sea long
+        long nL=12345678901L;
+
+        double nD=123.4567890123;
+        //Se coloca la F al final para que sea float
+        float nf=123.4567890123F;
+
+        //A partir de Java 10 no es necesario ingresar el tipo de variable
+        var cadena = "name";
+
 
     }
 }
